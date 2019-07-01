445156d2a87833bd6d6946c776a9cbb306de8ae7
Rob Paik <robpaik@gmail.com>
2019-07-01 01:58:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   new_array = []
     while counter < source_array.length do
       new_array[counter] = -1 * source_array[counter]
-      counter++
+      counter+=1
     end
   return new_array
 end
