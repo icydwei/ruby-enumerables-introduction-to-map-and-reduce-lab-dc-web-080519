322f7b8fd7e0b12e63df21f27375c239046349e2
Rob Paik <robpaik@gmail.com>
2019-07-01 02:01:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,23 @@ def map_to_no_change(source_array)
     end
   return nochange_array
 end
+
+def map_to_double(source_array)
+  counter = 0
+  double_array = []
+  while counter < source_array.length do
+      double_array[counter] = source_array[counter] * 2
+      counter+=1
+    end
+  return double_array
+end
+
+def map_to_square(source_array)
+  counter = 0
+  square_array = []
+  while counter < source_array.length do
+      square_array[counter] = source_array[counter] ** source_array[counter]
+      counter+=1
+    end
+  return square_array
+end
