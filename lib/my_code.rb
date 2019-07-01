a608dc1364224e7314fafb418b4c6b8684a54a53
Rob Paik <robpaik@gmail.com>
2019-07-01 02:57:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,9 +52,9 @@ def reduce_to_all_true(source_array)
   counter = 0
   while counter < source_array.length do
     if (source_array[counter] == false || source_array[counter] == nil)
-      counter += 1
-    elsif source_array[counter] == false
       return false
+    end
+      counter += 1
     end
   end
   return true
