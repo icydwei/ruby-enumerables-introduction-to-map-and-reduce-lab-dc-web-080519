926d76052fc1a9860883e70274c9eb4fa24e0c25
Rob Paik <robpaik@gmail.com>
2019-07-01 02:23:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,4 +47,9 @@ def reduce_to_total(source_array, starting_point=0)
   end
   return sum
 end
-  
+
+def reduce_to_all_true(source_array)
+  counter = 0
+  while counter < source_array.length do
+    if (source_array[counter] == )
+  end
